d7384c9db4da2c65713b5c5729be1e4266e3ba99
Andrew Bloom <blooma116@gmail.com>
2018-04-09 16:08:14 -0700

    setting up react skeleton

---
+++ src/component/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <div>
+          <header>
+            <h1>Hello!</h1>
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('hello');
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './component/app';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDom.render(<App/>, container);
